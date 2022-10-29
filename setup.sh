9ab6ebc7d3d9597835a6b558a8c05b1a2b223294
Maxim Kurin <kurinmaksim42@gmail.com>
2022-10-29 08:49:29 +0300

    Add full install

--- setup.sh
+++ setup.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# exit when any command fails
+set -e
+
+sudo apt update
+sudo apt install zsh git fonts-font-awesome
+
 # config dir
 export CONFIG_DIR=$HOME/.config
 export PROGRAMS_DIR=$HOME/programs
 
 echo "#!/bin/sh\n\nsource \"$CONFIG_DIR/.config/zsh/.zshrc\"" > $HOME/.zshrc
 
+mkdir -p $PROGRAMS_DIR
 [[ ! -d $PROGRAMS_DIR/powerlevel10k ]] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $PROGRAMS_DIR/powerlevel10k
 
 mkdir -p $CONFIG_DIR
 cp -r ./zsh $CONFIG_DIR
 cp -r ./p10k $CONFIG_DIR
+
+chsh -s $(which zsh)
+
+echo "Please reboot the PC"
